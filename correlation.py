592713841c60506cbc731118754fbc0d5358fe05
Charush Taneja <43379186+Cat-taC@users.noreply.github.com>
2020-07-12 22:05:43 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,22 +1,23 @@
 import nsepy
 import datetime
 import pandas as pd
-
-excel = pd.read_csv("G:/Stock/Symbols.csv")
+#Importing Data from csv file for list of stocks with header as Symbol
+excel = pd.read_csv("G:/Stock/Symbols.csv") 
 
 symbol = excel[["Symbol"]]
 
-for each_symbol in symbol['Symbol']:    
+for each_symbol in symbol['Symbol']:    #looping through each symbol
+    #getting data of symbol for the mentioned dates
     stock = nsepy.get_history(each_symbol,
                 start=datetime.date(2019,1,1), 
                 end=datetime.date(2019,12,31))
-    for symbol2 in symbol['Symbol']:
-        if(each_symbol > symbol2 ):
+    for symbol2 in symbol['Symbol']: #getting second symbol
+        if(each_symbol > symbol2 ): 
             stock2 = nsepy.get_history(symbol2,
                             start=datetime.date(2019,1,1), 
                             end=datetime.date(2019,12,31))
-            corel = stock['Close'].corr(stock2['Close'])
-            if(corel > 0.5 or corel < -0.5):
+            corel = stock['Close'].corr(stock2['Close']) #Co-relation value between Close Price of 2 stocks
+            if(corel > 0.5 or corel < -0.5): #Printing only if Co-relation value is greater than 0.5 or -0.5
                 print(each_symbol + " " + symbol2 + " " + str(corel))
 
     
